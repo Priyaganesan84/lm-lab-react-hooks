08dd6dad76fb6265e129ab763336e71323352138
Priyaganesan84 <136799329+Priyaganesan84@users.noreply.github.com>
2023-09-02 18:15:06 +0100

    UseState Impl

--- src/components/hooks/use_state/count_cats.tsx
+++ src/components/hooks/use_state/count_cats.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
+
 export function CountCats() {
+
+  //initialise the state for countCat
+  const[catCount,setCatCount] =useState(0);
 
   const cats = ["🐈"].length;
 
-  return (
+  //function to increment catcount
+  function handleClick() {
+    setCatCount(catCount + 1);
+  }
+
+    return (
     <>
     <h2>useState</h2>
 
-    <p>{cats}</p>
-
-    <button>
-      There are ... cats 🥳
-    </button>
+    <p>Number of cats : {cats}</p>
+  
+    <button onClick={handleClick}> Adding cats ..... There are ... {catCount} cats 🥳 </button>
+    <p>{catCount}  cats 🐈 </p>
     </>
   );
 }
