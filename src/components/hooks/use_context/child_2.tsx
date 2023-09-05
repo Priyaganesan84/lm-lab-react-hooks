a912fda7f2ba0968827570b20a48d5a649b52dc4
Priyaganesan84 <136799329+Priyaganesan84@users.noreply.github.com>
2023-09-05 18:28:48 +0100

    user_context Impl

--- src/components/hooks/use_context/child_2.tsx
+++ src/components/hooks/use_context/child_2.tsx
@@ -1,10 +1,26 @@
-export const Child2: React.FC<{ dark: boolean }> = ({ dark }) => {
-	const theme = {
-		backgroundColor: dark ? '#333' : '#CCC',
-		color: dark ? 'white' : '#333',
-		padding: '2rem',
-		margin: '2rem',
-	};
+import  { useContext } from 'react';
+import { ThemeContext } from './parent';
 
-	return <div style={theme}>Child 2</div>;
+export const Child2 = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <div style={theme}>
+      <h3>Child 2</h3>
+      <p>This is Child 2.</p>
+    </div>
+  );
 };
+
+
+//initial code can be removed
+// export const Child2: React.FC<{ dark: boolean }> = ({ dark }) => {
+// 	const theme = {
+// 		backgroundColor: dark ? '#333' : '#CCC',
+// 		color: dark ? 'white' : '#333',
+// 		padding: '2rem',
+// 		margin: '2rem',
+// 	};
+
+// 	return <div style={theme}>Child 2</div>;
+// };
--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,6 +1,25 @@
-import { useState } from 'react';
+import { useState,createContext } from 'react';
 import { Child1 } from './child_1';
 import { Child2 } from './child_2';
+
+
+// Define the Theme type
+interface Theme  {
+    backgroundColor: string;
+    color: string;
+    padding: string;
+    margin: string;
+  };
+  
+
+// Defining the ThemeContext  in Parent.tsx
+export const ThemeContext = createContext<Theme>({
+    backgroundColor: '',
+    color: '',
+    padding: '',
+    margin: '',
+  });
+
 
 export const Parent = () => {
 
@@ -10,12 +29,29 @@ export const Parent = () => {
         setDarkTheme(prevDarkTheme => !prevDarkTheme)
     }
 
+    const theme : Theme = darkTheme
+    ? {
+        backgroundColor: 'black',
+        color: 'green',
+        padding: '1rem',
+		margin: '1rem',
+      }
+    : {
+        backgroundColor: 'green',
+        color: 'black',
+        padding: '1rem',
+		margin: '1rem',
+      };
+
+
     return (
         <div className='section'>
             <h2>useContext</h2>
             <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
+            <ThemeContext.Provider value ={theme}>
+            <Child1 />
+            <Child2 />
+            </ThemeContext.Provider>
         </div>
 
     )
