afddce436536d5693d724e5c9cad3e2991393ff8
Priyaganesan84 <136799329+Priyaganesan84@users.noreply.github.com>
2023-09-05 18:28:38 +0100

    user_context impl

--- src/components/hooks/use_context/child_1.tsx
+++ src/components/hooks/use_context/child_1.tsx
@@ -1,10 +1,25 @@
-export const Child1: React.FC<{ dark: boolean }> = ({ dark }) => {
-	const theme = {
-		backgroundColor: dark ? '#333' : '#CCC',
-		color: dark ? 'white' : '#333',
-		padding: '2rem',
-		margin: '2rem',
-	};
+import  { useContext } from 'react';
+import { ThemeContext } from './parent'; 
 
-	return <div style={theme}>Child 1</div>;
+export const Child1 = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <div style={theme}>
+      <h3>Child 1</h3>
+      <p>This is Child 1.</p>
+    </div>
+  );
 };
+
+//initial code can be removed
+// export const Child1: React.FC<{ dark: boolean }> = ({ dark }) => {
+// 	const theme = {
+// 		backgroundColor: dark ? '#333' : '#CCC',
+// 		color: dark ? 'white' : '#333',
+// 		padding: '2rem',
+// 		margin: '2rem',
+// 	};
+
+// 	return <div style={theme}>Child 1</div>;
+// };
