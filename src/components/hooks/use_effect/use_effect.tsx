55e366b3a33650219f9e98b27a0cd00b7ecbf403
Priyaganesan84 <136799329+Priyaganesan84@users.noreply.github.com>
2023-09-05 17:21:28 +0100

    useeffect impl

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,69 @@
 // make an api call using useEffect
 
+import React, { useState, useEffect } from 'react';
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+} 
+
 export const APICall = () => {
+  // Step 2: Create state using useState
+  const [todo, setTodo] = useState<Todo | null>(null);
 
-    // Your code here!
+  // Step 3: Use useEffect to make the API call
+  useEffect(() => {
+    // Step 4: Define an asynchronous function to make the API call
+    async function fetchTodo() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
+        // Step 5: Update the state with the fetched data
+        setTodo(data);
+      } catch (error) {
+        console.error('Error fetching todo:', error);
+      }
+    }
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+    // Call the fetchTodo function
+    fetchTodo();
+  }, []); // Empty dependency array means the effect runs once after the initial render
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      {/* Step 6: Display the fetched todo data */}
+      {todo ? (
+        <p>{todo.title} {todo.id} {todo.userId}</p>
+      
+      ) : (
+        <p>Loading todo...</p>
+      )}
+    </>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
